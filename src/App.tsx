24fca3419a0ce2903c8acaa5b1510c38a8968ad9
Dima Rosmait <>
2022-01-28 09:58:43 +0100

    Fist solution.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,114 @@
-import React from "react";
+import React, { ChangeEventHandler, useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  const step = 5;
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [repos, setRepos] = useState<any>({});
+  const [debouncedSearch] = useDebounce(search, 200);
+
+  useEffect(() => {
+    if (!debouncedSearch) {
+      setPage(1);
+      setRepos({});
+      return;
+    }
+
+    fetch(
+      `https://api.github.com/search/repositories?q=${debouncedSearch}&per_page=${step}&page=${page}`
+    )
+      .then((res) => res.json())
+      .then((data) =>
+        setRepos((value: any) => {
+          return page === 1
+            ? data
+            : {
+                ...value,
+                ...data,
+                items: [...(value.items || []), ...data.items],
+              };
+        })
+      );
+  }, [debouncedSearch, page]);
+
+  const onChange: ChangeEventHandler = ({ target }) => {
+    setSearch((target as HTMLInputElement).value);
+  };
+
+  const loadMore = () => {
+    setPage((value) => value + 1);
+  };
+
+  const resetSearch = () => {
+    setSearch("");
+  };
+
+  return (
+    <div className="app-wrapper">
+      <div className="content">
+        <div className="search">
+          <input
+            className="search-input"
+            type="text"
+            value={search}
+            onChange={onChange}
+          />
+          {debouncedSearch && (
+            <button className="search-reset" onClick={resetSearch}>
+              ❌
+            </button>
+          )}
+        </div>
+
+        <div>
+          {repos.total_count > 0 && (
+            <div>
+              <div className="card-list">
+                {repos.items.map((item: any) => (
+                  <a key={item.id} href={item.url}>
+                    <div className="card">
+                      <div className="card__first-line">
+                        <div className="card__avatar">
+                          <img
+                            src={item.owner.avatar_url}
+                            alt={item.owner.login}
+                          />
+                        </div>
+
+                        <h3 className="card__title">{item.name}</h3>
+
+                        <div className="card__stars">
+                          {item.stargazers_count} ⭐️{" "}
+                        </div>
+                      </div>
+
+                      <div className="card__description u-line-clamp-2">
+                        {item.description}
+                      </div>
+                    </div>
+                  </a>
+                ))}
+              </div>
+
+              <div className="count">
+                {page * step} / {repos.total_count}
+              </div>
+
+              <div>
+                <button className="btn" onClick={loadMore}>
+                  Load more
+                </button>
+              </div>
+            </div>
+          )}
+          {repos.total_count === 0 && <div>Not found</div>}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
